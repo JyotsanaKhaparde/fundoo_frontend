d9bcb41e030e31151854b860965397f011f2a702
JyotsanaKhaparde <41262339+JyotsanaKhaparde@users.noreply.github.com>
2019-02-07 10:47:01 +0530

    comment added in logininputtest

--- src/components/ __tests__ /LoginInput.test.js
+++ src/components/ __tests__ /LoginInput.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//it will mock our component rendering in the DOM
 import { shallow } from 'enzyme';
 import LoginInput from '../../components/logininput'
 describe('LoginInput component', () => {
